b22bf7e96d13719c4a7e0e50c8b478c756901b27
renner <renner0@posteo.de>
2025-10-26 17:09:34 +0100

    fix: undo bootc updates fuckery

--- build_files/04-misc.sh
+++ build_files/04-misc.sh
@@ -17,6 +17,4 @@ ln -s /usr/bin/nvim /usr/bin/vi
 systemctl enable podman-auto-update.timer
 systemctl --global enable podman-auto-update.timer
 
-sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
-sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
